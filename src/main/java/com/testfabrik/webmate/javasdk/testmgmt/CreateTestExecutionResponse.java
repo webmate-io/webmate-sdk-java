d1e50e15cbdf6b4e92ccb1335f9072caa627f2e6
Michael Mirold <mmirold@testfabrik.com>
2020-08-28 14:57:10 +0200

    Fix for changed createTestExecution API WMKNBN-6045

--- src/main/java/com/testfabrik/webmate/javasdk/testmgmt/CreateTestExecutionResponse.java
+++ src/main/java/com/testfabrik/webmate/javasdk/testmgmt/CreateTestExecutionResponse.java
@@ -9,23 +9,23 @@ import com.testfabrik.webmate.javasdk.JacksonMapper;
 /**
  * Contains TestExecutionId and optionally TestRunId.
  */
-class CreateTestExecutionResponse {
+public class CreateTestExecutionResponse {
     public final TestExecutionId executionId;
 
     public final Optional<TestRunId> optTestRunId;
 
-    CreateTestExecutionResponse(TestExecutionId executionId, Optional<TestRunId> optTestRunId) {
+    public CreateTestExecutionResponse(TestExecutionId executionId, Optional<TestRunId> optTestRunId) {
         this.executionId = executionId;
         this.optTestRunId = optTestRunId;
     }
 
     @JsonCreator()
     public static CreateTestExecutionResponse fromJson(JsonNode json) {
-        TestExecutionId executionId = TestExecutionId.of(json.get("textExecutionId").asText());
+        TestExecutionId executionId = TestExecutionId.of(json.get("testExecutionId").textValue());
         Optional<TestRunId> testRunId = Optional.absent();
 
         if (json.has("testRunId")) {
-            testRunId = Optional.of(TestRunId.of(json.get("testRunId").asText()));
+            testRunId = Optional.of(TestRunId.of(json.get("testRunId").textValue()));
         }
         return new CreateTestExecutionResponse(executionId, testRunId);
     }
