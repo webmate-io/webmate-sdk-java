d309aa60dfb5e5fdb3118c389218911a39ef55b3
Timo Gühring <tguehring@testfabrik.com>
2021-03-25 14:58:53 +0100

    Add @JsonIgnoreProperties annotation to TestRunInfo

--- src/main/java/com/testfabrik/webmate/javasdk/testmgmt/TestRunInfo.java
+++ src/main/java/com/testfabrik/webmate/javasdk/testmgmt/TestRunInfo.java
@@ -1,6 +1,7 @@
 package com.testfabrik.webmate.javasdk.testmgmt;
 
 import com.fasterxml.jackson.annotation.JsonAlias;
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
 import com.fasterxml.jackson.annotation.JsonProperty;
 import com.fasterxml.jackson.databind.JsonNode;
 import com.testfabrik.webmate.javasdk.ProjectId;
@@ -17,6 +18,7 @@ import java.util.Optional;
 /**
  * Identifies TestRun in a Test.
  */
+@JsonIgnoreProperties(ignoreUnknown = true)
 public class TestRunInfo {
 
     private TestRunId testRunId;
