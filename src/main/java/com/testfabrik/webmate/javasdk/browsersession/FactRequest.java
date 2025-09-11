9d0a3c1cfced4441d7acec70c7985d14bbc26180
Michael Mirold <mmirold@testfabrik.com>
2025-09-11 16:48:14 +0200

    Implement fact request specification in BrowserSessionStateExtractionConfig WMKNBN-11444

--- src/main/java/com/testfabrik/webmate/javasdk/browsersession/FactRequest.java
+++ src/main/java/com/testfabrik/webmate/javasdk/browsersession/FactRequest.java
@@ -1,10 +1,41 @@
 package com.testfabrik.webmate.javasdk.browsersession;
 
 public class FactRequest {
-    private FactType type;
+    private FactType factType;
+    private FactParams params;
 
     public FactRequest(FactType type) {
-
-        this.type = type;
+        this.factType = type;
     }
+
+    public FactRequest(FactType type, FactParams params) {
+        this.factType = type;
+        this.params = params;
+    }
+
+    public FactType getFactType() {
+        return factType;
+    }
+
+    public void setFactType(FactType type) {
+        this.factType = type;
+    }
+
+    public FactParams getParams() {
+        return params;
+    }
+
+    public void setParams(FactParams params) {
+        this.params = params;
+    }
+
+    @Override
+    public String toString() {
+        return "FactRequest{" +
+                "type=" + factType +
+                ", params=" + params +
+                '}';
+    }
+
+
 }
--- src/main/java/com/testfabrik/webmate/javasdk/browsersession/FactType.java
+++ src/main/java/com/testfabrik/webmate/javasdk/browsersession/FactType.java
@@ -1,9 +1,49 @@
 package com.testfabrik.webmate.javasdk.browsersession;
 
-public class FactType {
-    private String name;
+import com.fasterxml.jackson.annotation.JsonCreator;
+import com.fasterxml.jackson.annotation.JsonValue;
+import com.fasterxml.jackson.databind.JsonNode;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.fasterxml.jackson.databind.node.ObjectNode;
+import com.testfabrik.webmate.javasdk.JacksonMapper;
 
-    public FactType(final String name) {
-        this.name = name;
+public class FactType {
+    private String artifactType;
+    private String findingType;
+
+    private FactType(final String artifactTypeName, final String findingTypeName) {
+        this.artifactType = artifactTypeName;
+        this.findingType = findingTypeName;
+    }
+
+    public static FactType fromArtifactType(String artifactTypeName) {
+        return new FactType(artifactTypeName, null);
+    }
+
+    public static FactType fromFindingType(String findingType) {
+        return new FactType(null, findingType);
+    }
+
+    @JsonValue
+    JsonNode toJson() {
+        ObjectMapper om = JacksonMapper.getInstance();
+        ObjectNode root = om.createObjectNode();
+        if (this.artifactType != null) {
+            root.put("artifactType", this.artifactType);
+        } else if (this.findingType != null) {
+            root.put("findingType", this.findingType);
+        }
+        return root;
+    }
+
+    @JsonCreator
+    static FactType fromJson(JsonNode node) {
+        if (node.has("artifactType")) {
+            return FactType.fromArtifactType(node.get("artifactType").asText());
+        } else if (node.has("findingType")) {
+            return FactType.fromFindingType(node.get("findingType").asText());
+        } else {
+            throw new IllegalArgumentException("Invalid FactType JSON: " + node.toString());
+        }
     }
 }
