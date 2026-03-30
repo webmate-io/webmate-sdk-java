9d4479191513bfb9ae7bee630b48a4ca5b417dcf
Christian Willms <cwillms@testfabrik.com>
2026-03-30 14:46:15 +0200

    improved error handling for getDevice

--- src/main/java/com/testfabrik/webmate/javasdk/WebmateApiClientException.java
+++ src/main/java/com/testfabrik/webmate/javasdk/WebmateApiClientException.java
@@ -4,11 +4,29 @@ package com.testfabrik.webmate.javasdk;
  * Generic Exception of webmate API client.
  */
 public class WebmateApiClientException extends RuntimeException {
+    private final Integer statusCode;
+
     public WebmateApiClientException(String errorMsg) {
         super(errorMsg);
+        this.statusCode = null;
     }
 
     public WebmateApiClientException(String errorMsg, Throwable e) {
         super(errorMsg, e);
+        this.statusCode = null;
+    }
+
+    public WebmateApiClientException(String errorMsg, int statusCode) {
+        super(errorMsg);
+        this.statusCode = statusCode;
+    }
+
+    public WebmateApiClientException(String errorMsg, int statusCode, Throwable e) {
+        super(errorMsg, e);
+        this.statusCode = statusCode;
+    }
+
+    public Integer getStatusCode() {
+        return statusCode;
     }
 }
